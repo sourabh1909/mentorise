fa616a2e89ed84fde2e8a7adc74ca6b74fcd8fa8
Sourabh1909 <sourabhphalatane19@gmail.com>
2026-04-29 02:02:15 +0530

    changes mailer

--- mailer.js
+++ mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 // ─── Create transporter ──────────────────────────────────────────────────────
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
